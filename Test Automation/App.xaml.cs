cee7e47b2ea2150a4004f79f365650eb65e1bd98
razzouk elias <razukalias@gmail.com>
2026-03-08 10:37:58 +0100

    solution creation phase

---
+++ Test Automation/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Test_Automation
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
